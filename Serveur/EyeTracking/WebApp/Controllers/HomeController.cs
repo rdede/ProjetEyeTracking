db6713efc4e0747dfe4f4a6b5ec3308c817918e3
Thomas Souffleux <thomas.souffleux@rhinos-groupe.com>
2021-12-08 15:06:39 +0100

    feat: ajout d'un nouveau projet asp .net core 6 avec la bonne interface

---
+++ Serveur/EyeTracking/WebApp/Controllers/HomeController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace WebApp.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        // Use Dictionary as a map.
+        private Dictionary<string, double> coordsImg1 = new Dictionary<string, double>();
+        private Dictionary<string, double> coordsImg2 = new Dictionary<string, double>();
+        private Dictionary<string, double> coordsImg3 = new Dictionary<string, double>();
+
+        public HomeController()
+        {
+            this.coordsImg1.Add("X", 175.0);
+            this.coordsImg1.Add("Y", 122.0);
+            this.coordsImg1.Add("XMax", 460.0);
+            this.coordsImg1.Add("YMax", 537.0);
+
+            this.coordsImg2.Add("X", 809.0);
+            this.coordsImg2.Add("Y", 122.0);
+            this.coordsImg2.Add("XMax", 1094.0);
+            this.coordsImg2.Add("YMax", 537.0);
+
+            this.coordsImg3.Add("X", 1443.0);
+            this.coordsImg3.Add("Y", 122.0);
+            this.coordsImg3.Add("XMax", 1728.0);
+            this.coordsImg3.Add("YMax", 537.0);
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+           
+            StartServer();
+
+            return View();
+        }
+
+        private void StartServer()
+        {
+
+            var host = Dns.GetHostEntry("localhost");
+            var ipAddress = host.AddressList[0];
+            var localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+            var listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // A Socket must be associated with an endpoint using the Bind method
+                listener.Bind(localEndPoint);
+                // Specify how many requests a Socket can listen before it gives Server busy response.
+                // We will listen 10 requests at a time
+                listener.Listen(10);
+
+                Console.WriteLine("Waiting for a connection...");
+                var handler = listener.Accept();
+
+
+                // Incoming data from the client.
+                string data = "";
+                byte[] bytes;
+
+                while (true)
+                {
+                    bytes = new byte[1024];
+                    int bytesRec = handler.Receive(bytes);
+                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                    if (data.IndexOf("<EOF>") > -1)
+                    {
+                        break;
+                    }
+                }
+
+                Console.WriteLine("Text received : {0}", data);
+
+                byte[] msg = Encoding.ASCII.GetBytes(data);
+                handler.Send(msg);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        [HttpPost]
+        public IActionResult FormPostValue()
+        {
+            return Content("Vous avez choisi l'information " + HttpContext.Request.Form["value"]);
+        }
+    }
+}
